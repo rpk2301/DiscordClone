3aad89f1bc61c1d6dd8de0a9da1857db4847a805
Ricky <rpk2301@vt.edu>
2023-11-10 16:33:27 -0500

    Invite Link Generated

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import {useEffect, useState} from "react";
+import {InviteModal} from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
 
@@ -19,6 +20,7 @@ export const ModalProvider = () => {
     return(
         <>
         <CreateServerModal/>
+            <InviteModal/>
         </>
     )
 }
