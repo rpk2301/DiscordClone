d76fd471ec664ce78256f1969244c60872025445
Ricky <rpk2301@vt.edu>
2023-11-09 20:22:58 -0500

    Making sure .env isnt tracked

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,15 +1,11 @@
-import { Button } from "@/components/ui/button";
+
 import {cn} from "@/lib/utils";
-
-
 const state = true;
 export default function Home() {
   return (
       <div>
-      <p className="text-3xl font-bold text-indigo-500">Discord Clone!</p>
-          <Button>
-              Click Me
-          </Button>
+      <p className="text-3xl font-bold text-indigo-500">This is a protected route, I only want logged in users here.</p>
+
 
       </div>
 
