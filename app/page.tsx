94b3b6c5fbaa3eb77f921e38263a41670fff38d8
Ricky <rpk2301@vt.edu>
2023-11-09 19:55:09 -0500

    Open Sans Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@/components/ui/button";
+import {cn} from "@/lib/utils";
 
+
+const state = true;
 export default function Home() {
   return (
-      ////Alligns our items in column's next to each other
       <div>
       <p className="text-3xl font-bold text-indigo-500">Discord Clone!</p>
-          <Button>Click Me</Button>
-    </div>
+          <Button>
+              Click Me
+          </Button>
+
+      </div>
+
+
+
+      //To write a state based off of const, write something like
+      //"bg-indigo-500", state && "bg-red-500 for true var
+
   )
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,9 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        customVar: "bg-indigo-500"
+          //Create Custom Variants on the React Component
+          //I am creating customVar for discord button
       },
       size: {
         default: "h-10 px-4 py-2",
